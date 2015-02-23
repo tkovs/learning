b2ce1867faf634423983e89d0180c75c4eb56c81
tkovs <victor.rdg@hotmail.com>
2015-02-23 01:08:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## LEARNING
+
+I'm learning haskell, so get ready to see horrible codes (whitout FP maybe D:).
+
+## Start
+
+    02/22/2015
---
+++ source/list.hs
@@ -0,0 +1,17 @@
+oddNumbers = [1, 3, 5, 7, 9]
+pairNumbers = [2, 4, 6, 8, 10]
+
+-- Adicionar no início da lista
+-- x valor
+-- y lista
+addBeginList x y = x : y
+
+-- Adicionar no fim da lista
+-- x lista
+-- y lista
+addEndList x y = x ++ y
+
+-- Elemento específico de uma lista
+-- x lista
+-- y elemento
+elementList x y = x !! y
