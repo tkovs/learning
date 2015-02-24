5c449b5fd0793b5edf995a2ea1cd2c5595d1a822
tkovs <victor.rdg@hotmail.com>
2015-02-24 12:25:19 +0000

    Addex ranges.hs. Fixed days function.

--- source/list-comprehension.hs
+++ source/list-comprehension.hs
@@ -4,6 +4,11 @@ basic x = [y | y <- [x..x+10]]
 -- Lista todos valores pares no intervalo de x a y
 pair x y = [s | s <- [x..y], s `mod` 2 == 0]
 
+
+-----------------------------------
+-- Fiz depois de ler isso
+-- https://www.facebook.com/groups/linguagemc/permalink/703023669795376/
+
 -- Informa se ano é bissexto ou não
 bis x = if (x `mod` 4 == 0) && (x `mod` 100 /= 0)
 			then 1
@@ -20,4 +25,9 @@ diy x = if bis x == 1
 -- Informa quantidade de dias que há somando determinados anos
 -- days [1892, 1896, 1900, 1904]
 -- ESTÁ ERRADO
-days x = sum [s | a <- x, s <- diy a]
+-- days x = sum [s | a <- x, s <- diy a]
+days x = sum [diy s | s <- x]
+-- ghci> days [1900, 1902, 1903, 1904, 1908, 2013]
+-- 2192
+-----------------------------------
+
