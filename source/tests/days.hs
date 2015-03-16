78b7d20709dd6f93b90160cbfe4031a7fa93fa5c
tkovs <victor.rdg@hotmail.com>
2015-03-16 07:41:53 -0300

    Work on days.hs (used guards)

--- source/tests/days.hs
+++ source/tests/days.hs
@@ -5,34 +5,23 @@
 -- 				then 1
 -- 				else 0
 
--- Retorna quantidade de dias no ano x
--- Days in year
--- diy x = if bis x == 1
--- 			then 366
--- 			else 365
-
--- Informa quantidade de dias que há somando determinados anos
--- days [1892, 1896, 1900, 1904]
--- ESTÁ ERRADO
--- days x = sum [s | a <- x, s <- diy a]
--- days x = sum [diy s | s <- x]
--- bissexto: 1904, 1908
--- comum: 1900, 1902, 1903, 2013
--- nem bissexto nem comum: seriously? are you crazy?
--- ghci> days [1900, 1902, 1903, 1904, 1908, 2013]
--- 2192
-
 -- Informa se ano é bissexto ou não
-bis x = if (x `mod` 4 == 0) && (x `mod` 100 /= 0)
-			then 1
-			else if (x `mod` 400 == 0)
-				then 1
-				else 0
+-- Retorna quantidade de dias no ano x
+-- diy x = if bis x == 1
+--			then 366
+--			else 365
+
+bis :: (Integral x) => x -> x
+bis x  
+    | x `mod` 4 == 0 && x `mod` 100 /= 0 = 1
+    | x `mod` 400 == 0 = 1
+    | otherwise   = 0
 
 -- Retorna quantidade de dias no ano x
-diy x = if bis x == 1
-			then 366
-			else 365
+diy :: (Integral x) => x -> x
+diy x
+	| bis x == 1 = 366
+	| otherwise  = 365
 
 -- Informa quantidade de dias que há somando determinados anos
 days x = sum [diy s | s <- x]
