b4f55b58aa83372142c0b76498dd15717bc260a9
tkovs <victor.rdg@hotmail.com>
2015-04-25 14:50:27 +0000

    New functions

--- source/list.hs
+++ source/list.hs
@@ -112,3 +112,29 @@ inverso [] = []
 
 inverso [1,2,3,4] = [4,3,2,1]
 -}
+
+-- Verifica se x lista possue y elemento
+possue :: [Int] -> Int -> Bool
+possue [] _ = False
+possue (x:xs) y
+    | x == y = True
+    | otherwise = possue xs y
+
+-- Retorna o maior valor de uma lista
+maior :: [Int] -> Int
+maior [x] = x
+maior (x:xs)
+    | x > maior xs = x
+    | otherwise = maior xs
+
+-- Verifica se é uma lista de números pares
+todos_pares :: [Int] -> Bool
+todos_pares [] = True
+todos_pares (x:xs)
+    | mod x 2 /= 0 = False
+    | otherwise = todos_pares xs
+
+-- Soma de uma lista de inteiros
+soma :: [Int] -> Int
+soma [] = 0
+soma (x:xs) = x + soma xs
