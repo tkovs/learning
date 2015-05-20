659dd3ff7befaaee3aa623c6ac3e8e98853d91da
tkovs <victor.rdg@hotmail.com>
2015-05-20 02:47:09 -0300

    New data type Image, more functions

--- Haskell/source/algebraic-data-type.hs
+++ Haskell/source/algebraic-data-type.hs
@@ -15,3 +15,29 @@ is_programmer _ = False
 is_student :: People -> Bool
 is_student (Student _ _) = True
 is_student _ = False
+
+type Line = [Char]
+type Image = [Line]
+
+-- Exemplo de imagem
+image :: Image
+image = [".......##...",
+         ".....##..#..",
+         "...##.....#.",
+         "..#.......#.",
+         "..#...#...#.",
+         "..#...#.#.#.",
+         "..#...#..##.",
+         "..#...#.....",
+         "...#...#....",
+         "....#...#...",
+         ".....#..#...",
+         "......###..."]
+
+-- Espelha imagem horizontalmente
+reverseH :: Image -> Image
+reverseH image = reverse image
+
+-- Espelha imagem verticalmente
+reverseV :: Image -> Image
+reverseV image = map reverse image
