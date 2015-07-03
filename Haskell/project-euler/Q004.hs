cd0f23aad931373960075d7e75a0bbe54207193a
tkovs <victor.rdg@hotmail.com>
2015-07-03 23:11:28 +0000

    Haskell . Project Euler Q004 -> Fixes range

--- Haskell/project-euler/Q004.hs
+++ Haskell/project-euler/Q004.hs
@@ -6,8 +6,10 @@ the product of two 2-digit numbers is 9009 = 91 × 99.
 Find the largest palindrome made from the product of two 3-digit numbers.
 -}
 
+import Data.List (sort, nub)
+
 numbers :: [Int]
-numbers = [a * b | a <- [10..99], b <- [a..99]]
+numbers = sort $ nub [a * b | a <- [100..999], b <- [a..999]]
 
 palindrome :: Int -> Bool
 palindrome n = (show n) == (reverse $ show n)
