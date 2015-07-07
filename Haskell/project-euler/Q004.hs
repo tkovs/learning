60badbffd78b3af52a948330d99ff133f5170de6
tkovs <victor.rdg@hotmail.com>
2015-07-07 19:50:24 +0000

    Haskell . Project Euler Q020 -> Improvement

--- Haskell/project-euler/Q004.hs
+++ Haskell/project-euler/Q004.hs
@@ -11,7 +11,7 @@ import Data.List (sort, nub)
 numbers :: [Int]
 numbers = sort $ nub [a * b | a <- [100..999], b <- [a..999]]
 
-palindrome :: Int -> Bool
+palindrome :: a -> Bool
 palindrome n = (show n) == (reverse $ show n)
 
 main = putStrLn $ show $ head $ filter (palindrome) $ reverse numbers
