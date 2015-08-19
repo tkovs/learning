b5f70bac2dcd13bcd9d31aceea9869f7c331f9eb
tkovs <victor.rdg@hotmail.com>
2015-08-19 15:43:44 +0000

    C . Challenge accepted (extreme.c) -> Improvement

--- C/src/extreme.c
+++ C/src/extreme.c
@@ -4,27 +4,46 @@
 #define MAX(a,b) a > b ? a : b
 #define MIN(a,b) a < b ? a : b
  
-int foo(int *x, int *y);
-int bar(int *x, int *y);
+int bigger(int*, int*);
+int smaller(int*, int*);
+int index(int, int*, int);
 
 int main(void)
 {
-   int x[SIZE] = {2, 3, 4, 10, 5, 1, 6, 7, 8, 9};
+   int array[SIZE] = {2, 3, 4, 10, 5, 1, 6, 7, 8, 9};
 
-   printf("Bigger: %d\n", foo(x, x + SIZE-1));
-   printf("Smaller: %d\n", bar(x, x + SIZE-1));
+   int b = bigger(array, array + SIZE-1);
+   int ib = index(b, array, SIZE);
+
+   int s = smaller(array, array + SIZE-1);
+   int is = index(s, array, SIZE);
+
+   printf ("Bigger: %d - Index: %d\n", b, ib);
+   printf ("Smaller: %d - Index: %d\n", s, is);
 
    return 0;
 }
 
-int foo(int *x, int *y)
+int index(int x, int *array, int size) {
+   int i = 0;
+
+   while (i < size) {
+      if (x == array[i])
+         return i;
+      else i++;
+   }
+
+   return -1;
+}
+
+int bigger(int *x, int *y)
 {
    if (x == y) { return *x; }
-   return MAX(*x, foo(x + 1, y));
+   return MAX(*x, bigger(x + 1, y));
 }
  
-int bar(int *x, int *y)
+int smaller(int *x, int *y)
 {
    if (x == y) { return *x; }
-   return MIN(*x, bar(x + 1, y));
+   return MIN(*x, smaller(x + 1, y));
 } 
