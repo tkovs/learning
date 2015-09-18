0d3f11bce58b21a81fda819ed5301baf03f07406
tkovs <victor.rdg@hotmail.com>
2015-09-18 01:08:16 +0000

    Arduino . Otimização do projeto Semaforo

--- Arduino/Semaforo/src/semaforo.ino
+++ Arduino/Semaforo/src/semaforo.ino
@@ -1,49 +1,38 @@
 int veiculo_verde = 8;
 int veiculo_amarelo = 9;
 int veiculo_vermelho = 10;
-int pedestre_verde = 11;
-int pedestre_vermelho = 12;
-
 int INICIO = 8;
-int FIM = 12;
+int FIM = 10;
 
 void setup() {
   pinMode(veiculo_verde, OUTPUT);
   pinMode(veiculo_amarelo, OUTPUT);
   pinMode(veiculo_vermelho, OUTPUT);
-  pinMode(pedestre_verde, OUTPUT);
-  pinMode(pedestre_vermelho, OUTPUT);
 }
 
 void loop() {
   acende(veiculo_verde);
-  acende(pedestre_vermelho);
-
   delay(4000);
 
   apaga(veiculo_verde);
   acende(veiculo_amarelo);
 
-  delay(2000);
+  delay(1000);
 
   apaga(veiculo_amarelo);
   acende(veiculo_vermelho);
-  
-  acende(pedestre_verde);
-  apaga(pedestre_vermelho);
 
   delay(4000);
 
   apaga(veiculo_vermelho);
-  apaga(pedestre_verde);
 }
 
 void acende(int ledPin) {
-  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin, 1);
 }
 
 void apaga(int ledPin) {
-  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin, 0);
 }
 
 void acende_geral() {
