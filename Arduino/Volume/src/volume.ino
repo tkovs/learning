ebb9e9dfc908791f8d10787753b9a09fe7e3d7d8
tkovs <victor.rdg@hotmail.com>
2015-09-18 20:11:38 +0000

    Arduino . Projeto solidariedade alterado para volume

---
+++ Arduino/Volume/src/volume.ino
@@ -0,0 +1,54 @@
+const int analogPino = A0;
+const int PINOS = 6;
+const int pinosLeds[PINOS] = {13, 12, 11, 10, 9, 8};
+const int brilho = 500;
+ 
+int valorSensor = 0;
+int indice = 0;
+ 
+/* Potenciômetro
+     GND  A0  5V
+      |   |   |
+0000 (=========) 1023
+         [=]
+         [=]
+         [=]
+         [=]
+*/
+ 
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  valorSensor = analogRead(analogPino);
+  indice = map(valorSensor, 0, 1023, -1, 5);
+ 
+  volume(indice);
+ 
+  delay(30);
+}
+ 
+void apagatudo() {
+  int i = 0;
+ 
+  while (i < PINOS)
+    analogWrite(pinosLeds[i++], 0);
+}
+
+
+void volume(int limite) {
+  int indice = 0;
+
+  if (limite < 0){
+    apagatudo();
+  } else {
+    while (indice <= limite) {
+      analogWrite(pinosLeds[indice++], brilho);
+    }
+
+    while (indice < PINOS) {
+      analogWrite(pinosLeds[indice++], 0);
+    }
+  }
+}
