9c0a06d56e497b1c6e2a0109e3f8df2b795769ca
tkovs <victor.rdg@hotmail.com>
2015-12-30 21:40:52 +0000

    Haskell . Programming in Haskell (book) -> Exercises 2.6 Q1

---
+++ Haskell/books/Programmming in Haskell/chapter2/exercises/q1.hs
@@ -0,0 +1,13 @@
+-- Parenthesise the following arithmetic expressions:
+
+{-  
+	QUESTIONS
+    2 ^ 3 * 4
+    2 * 3 + 4 * 5
+    2 + 3 * 4 ^ 5
+
+    ANSWERS
+    (2 ^ 3) * 4
+    (2 * 3) + (4 * 5)
+    2 + (3 * (4 ^ 5))
+-}
