598a7221bc9b23dd84e4af17c6fe5f2cf9ebef10
tkovs <victor.rdg@hotmail.com>
2016-01-02 22:47:08 +0000

    Haskell . Programming in Haskell (book) -> Exercises 3.11 Q2

---
+++ Haskell/books/Programmming in Haskell/chapter3/exercises/q2.hs
@@ -0,0 +1,31 @@
+{-
+	What are the types of the following functions?
+
+	second xs = head (tail xs)
+	swap (x,y) = (y,x)
+	pair x y = (x,y)
+	double x = x * 2
+	palindrome xs = reverse xs == xs
+	twice f x = f (f x)
+
+	Hint: take care to include the necessary class constraints if the functions
+	are defined using overloaded operators.
+-}
+
+second :: [a] -> a
+second xs = head (tail xs)
+
+swap :: (a,b) = (b,a)
+swap (x,y) = (y,x)
+
+pair :: a -> b -> (a,b)
+pair x y = (x,y)
+
+double :: Num a => a -> a
+double x = x * 2
+
+palindromee :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
+
+twice :: (f -> a) -> a -> a
+twice f x = f (f x)
