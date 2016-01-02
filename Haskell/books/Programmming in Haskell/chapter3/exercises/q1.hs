57796cda31e5106ea1ff2f5a4c31981cca17984a
tkovs <victor.rdg@hotmail.com>
2016-01-02 22:38:22 +0000

    Haskell . Programming in Haskell (book) -> Exercises 3.11 Q1

---
+++ Haskell/books/Programmming in Haskell/chapter3/exercises/q1.hs
@@ -0,0 +1,17 @@
+{-
+	What are the types of the following values?
+
+	1. ['a','b','c']
+	2. ('a','b','c')
+	3. [(False,'O'),(True,'1')]
+	4. ([False,True], ['0','1'])
+	5. [tail,init,reverse]
+
+	ANSWERS
+
+	1. [Char]
+	2. (Char,Char,Char)
+	3. [(Bool, Char)]
+	4. ([Bool], [Char])
+	5. [[a] -> [a]]
+-}
