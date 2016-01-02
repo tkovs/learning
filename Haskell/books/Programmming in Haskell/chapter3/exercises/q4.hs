57bf99a96d5112df93dea9b686bc240dc12e1727
tkovs <victor.rdg@hotmail.com>
2016-01-02 23:11:06 +0000

    Haskell . Programming in Haskell (book) -> Exercises 3.11 Q4

---
+++ Haskell/books/Programmming in Haskell/chapter3/exercises/q4.hs
@@ -0,0 +1,9 @@
+{-
+	Why is it not feasible in general for function types to be instances of the
+	Eq class? When is it feasible? Hint: two functions of the same type are
+	equal if they always return equal results for equal arguments.
+
+	ANSWER
+	oh my fucking god, it would give so much work, it's not feasible coz it can
+	receive infinite possible arguments.
+-}
