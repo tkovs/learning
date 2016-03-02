cf903c5efc727da4f50ab78d68d80a74ba8338c8
tkovs <victor.rdg@hotmail.com>
2016-03-02 17:36:31 -0300

    Ruby . Aprenda a programar - 3. Variaveis

---
+++ Ruby/learn to program/03 Variaveis/tipos.rb
@@ -0,0 +1,5 @@
+var = 'só mais uma ' + 'string'
+puts var
+
+var = 5 * (2 + 3)
+puts var
---
+++ Ruby/learn to program/03 Variaveis/variar.rb
@@ -0,0 +1,5 @@
+compositor = 'Mozart'
+puts compositor + ' era "sensa", na sua época.'
+
+compositor = 'Beethoven'
+puts 'Mas eu, pessoalmente, prefiro ' + compositor + '.'
