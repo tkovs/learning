61018b1d0730e24ea9b15baf13c2f2b379e0c0ca
tkovs <victor.rdg@hotmail.com>
2016-03-05 11:41:48 -0300

    Ruby . Aprenda a programar - 6. Controle de fuxo (condicionais)

--- Ruby/learn to program/06 Controle de Fluxo/if.rb
+++ Ruby/learn to program/06 Controle de Fluxo/if.rb
@@ -2,8 +2,10 @@ puts 'Olá! Eu sou vidente. Qual é o seu nome?'
 nome = gets.chomp
 puts 'Olá, ' + nome
 
-if nome == 'João'
+if nome == 'João' then
 	puts 'Vejo coisas incríveis no seu futuro!'
+elsif nome == 'Vitor' then
+	puts 'Ok.. É um futuro maneiro, digamos assim'
 else
 	puts 'Seu futuro é... Ó, Deus! Olha a hora!'
 	puts 'Eu tenho que ir, mil perdões!'
