511fbd5feeda338b9e517ed3370db1abbeb0e215
tkovs <victor.rdg@hotmail.com>
2016-03-06 19:07:07 -0300

    Ruby . Aprenda a programar - 8. Metodos

---
+++ Ruby/learn to program/08 Metodos/linguagem.rb
@@ -0,0 +1,7 @@
+def test(a = 'Ruby', b = 'Perl')
+    puts 'The programming language is ' + a
+    puts 'The programming language is ' + b
+end
+
+test('C++')
+test
---
+++ Ruby/learn to program/08 Metodos/variavellocal.rb
@@ -0,0 +1,7 @@
+def test(x)
+    i = x + 1
+    puts i
+end
+
+#undefined local variable or method `i' for main:Object (NameError)
+puts i
