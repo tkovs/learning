6f846ceee3d65684954d5bb95d145c038c7f0888
tkovs <victor.rdg@hotmail.com>
2016-03-13 21:55:10 -0300

    Soluções para project-euler escritas em Ruby

--- Haskell/project-euler/Q003.hs
+++ Haskell/project-euler/Q003.hs
@@ -10,7 +10,7 @@ is_prime :: Int -> Bool
 is_prime n = (length $ divisors n) == 1
 
 divisors :: Int -> [Int]
-divisors n = (1:) $ nub $ concat [[x, div n x] | x <- [2..limit], rem n x == 0 ]
+divisors n = (1:) $ nub $ concat [[x, div n x] | x <- [2..limit], rem n x == 0]
      where limit = (floor.sqrt.fromIntegral) n
 
 main = putStrLn $ show $ last $ filter (is_prime) $ divisors 600851475143
