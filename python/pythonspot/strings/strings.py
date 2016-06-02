2019d62f327533463112950a28cd6e07d67072a2
tkovs <victor.rdg@hotmail.com>
2016-06-02 15:55:46 -0300

    Python . Strings

---
+++ python/pythonspot/strings/strings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Para reconhecer caracteres UTF-8, usa-se a linha abaixo, como diz a documentação
+
+nome = "Joao Vitor Rodrigues da Silva"
+dia = "Quinta"
+
+print("Meu nome é %s e hoje é %s") % (nome, dia)
+print(dia[2], dia[3]); # 0..n
+print("Nome nao completo: " + nome[5:10])
