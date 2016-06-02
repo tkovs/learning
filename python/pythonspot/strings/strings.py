219a338fbd34a31e45e6ec2322c4b9252986fb89
tkovs <victor.rdg@hotmail.com>
2016-06-02 16:21:04 -0300

    Python . String - Função format e %

--- python/pythonspot/strings/strings.py
+++ python/pythonspot/strings/strings.py
@@ -4,6 +4,9 @@
 nome = "Joao Vitor Rodrigues da Silva"
 dia = "Quinta"
 
-print("Meu nome é %s e hoje é %s") % (nome, dia)
+# O operador % usado abaixo está meio que obsoleto
+# print("Meu nome é %s e hoje é %s") % (nome, dia)
+# No lugar dele, usa-se a função format
+print("Meu nome é {0} e hoje é {1}".format(nome, dia))
 print(dia[2], dia[3]); # 0..n
 print("Nome nao completo: " + nome[5:10])
