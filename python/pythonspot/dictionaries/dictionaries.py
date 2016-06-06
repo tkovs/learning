9ab0a708b1b28c915dccae17b083e16ab39ee6fb
tkovs <victor.rdg@hotmail.com>
2016-06-06 16:52:01 -0300

    Python . Dictionaries

---
+++ python/pythonspot/dictionaries/dictionaries.py
@@ -0,0 +1,9 @@
+words = {}
+
+words["Hello"] = "Bonjour"
+words["Yes"] = "Oui"
+words["No"] = "Non"
+words["Bye"] = "Au Revoir"
+
+print(words["Hello"])
+print(words["No"])
---
+++ python/pythonspot/dictionaries/number.py
@@ -0,0 +1,8 @@
+dict = {}
+dict['Ford'] = "Car"
+dict['Python'] = "The Python Programming Language"
+dict[2] = "This sentence is stored here."
+ 
+print(dict['Ford'])
+print(dict['Python'])
+print(dict[2])
