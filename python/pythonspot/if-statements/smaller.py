2737fd239a13637edaf153793f6525c5514f8a4e
tkovs <victor.rdg@hotmail.com>
2016-06-07 15:25:34 -0300

    Python . Decisões (if)

---
+++ python/pythonspot/if-statements/smaller.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+inteiro = int(input("Insira um número: "))
+
+if (inteiro < 100):
+    print("Menor que 100!")
+elif (inteiro == 100):
+    print("Igual a 100")
+else:
+    print("Maior que 100")
+
