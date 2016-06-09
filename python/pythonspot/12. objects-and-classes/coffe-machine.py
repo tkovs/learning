0306bf4e57d3be745f3627e0fbb971d1a270b93e
tkovs <victor.rdg@hotmail.com>
2016-06-09 14:37:23 -0300

    Python . Objetos e Classes - Finalizando tutorial

--- python/pythonspot/12. objects-and-classes/coffe-machine.py
+++ python/pythonspot/12. objects-and-classes/coffe-machine.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 class CoffeMachine:
     name = ""
     beans = 0
@@ -9,15 +11,24 @@ class CoffeMachine:
         self.water = water
 
     def addBean(self):
-        self.beans = self.beans + 1
+        self.beans += 1
 
     def removeBean(self):
-        self.beans = self.beans - 1
+        self.beans -= 1
 
     def addWater(self):
-        self.water = self.water + 1
+        self.water += 1
 
     def removeWater(self):
-        self.water = self.water - 1
+        self.water -= 1
 
     def printState(self):
+        print("Name ... {0}".format(self.name))
+        print("Beans .. {0}".format(self.beans))
+        print("Water .. {0}\n".format(self.water))
+
+pythonBean = CoffeMachine("Python Bean", 83, 20)
+pythonBean.printState()
+pythonBean.addBean()
+pythonBean.printState()
+
