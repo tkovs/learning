3f662988eeb3876b15768e133c3cd653b52f9994
tkovs <victor.rdg@hotmail.com>
2016-06-20 16:17:01 -0300

    Python . Conversão de resultados para o formato lista

--- python/pythonspot/24. lambda/functions.py
+++ python/pythonspot/24. lambda/functions.py
@@ -1,8 +1,10 @@
-list = [1,2,3,4,5,6,7,8,9,10]
+from functools import reduce
 
-squaredList = map(lambda x: x*x, list)
-newList = filter(lambda x: x % 2 == 0, list)
-reducedList = reduce(lambda x,y: x + y, list)
+initial_list = [1,2,3,4,5,6,7,8,9,10]
+
+squaredList = list(map(lambda x: x*x, initial_list))
+newList = list(filter(lambda x: x % 2 == 0, initial_list))
+reducedList = reduce(lambda x,y: x + y, initial_list)
 
 print("Squared list ... {0}".format(squaredList))
 print("Even list ...... {0}".format(newList))
