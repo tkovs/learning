b1f918eadb3dbdac5b5babee069fad7aeea72641
tkovs <tkovs@outlook.com>
2017-10-06 17:36:54 -0300

    PHP . Comentários e tipos de dados

---
+++ php/comments.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+	    <title>Comments</title>
+	</head>
+	<body>
+		<?php
+		#  Comentário de uma linha com sharp
+
+		// Comentário de uma linha com duas barras
+
+		/* Comentário multilinha
+		muito útil */
+
+		echo "Exibindo formas de comentários"; # Comentário
+		?>
+	</body>
+</html>
---
+++ php/typedata.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+	    <title>Hello, world!</title>
+	</head>
+	<body>
+		<?php
+		$idade = 20; # Integer
+		$ano_nascimento = 1997; # Integer
+
+		$pi = 3.14; # Double
+		$altura = 1.81; # Double
+
+		/*
+			Se um valor é um número, ele é falso somente se for igual a 0.
+			Se for string, é falso se a string tiver 0 caracters ou for "0".
+			Se for NULL é sempre falso.
+			Se for array, é falso se não tiver valores.
+			Não usar double como boolean.
+		*/
+		$vivendo = TRUE;
+		$morto = FALSE;
+
+		if ($morto) {
+			printf("Ué");
+		} else if ($vivendo) {
+			printf("Vivo!!<br />");
+		}
+		$aspas_duplas = "Testando<br />";
+		$aspas_simples = 'Testando<br />';
+
+		$nome = "João Vitor";
+		$literalmente = 'Meu nome é $nome.<br />';
+		print($literalmente);
+		$literalmente = "Meu nome é $nome.<br />";
+		print($literalmente);
+		?>
+	</body>
+</html>
