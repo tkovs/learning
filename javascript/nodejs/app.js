e5c5aa5e8bf813a2d709aab175c02b8d036a1448
tkovs <tkovs@outlook.com>
2018-07-18 16:30:20 -0300

    Javascript . NodeJS, Mini servidor

--- javascript/nodejs/app.js
+++ javascript/nodejs/app.js
@@ -1,6 +1,27 @@
-console.log('Hello World');
-var a = 2;
-var b = 2;
-var c = a + b;
+var http = require('http');
 
-console.log('c =', c);
+function handle(request, response)
+{
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    response.write("<!DOCTYPE html>");
+    response.write("<html>");
+    response.write("<head>");
+    response.write("<meta charset='utf-8'>");
+    response.write("<title>Resposta</title>");
+    response.write("</head>");
+    response.write("<body>");
+    response.write("<h2>Página de resposta</h2>");
+    response.write("</body>");
+    response.write("</html>");
+
+    response.end();
+}
+
+var server = http.createServer(handle);
+
+server.listen(45000, function () {
+    console.log('Server is listening at 45000 port');
+});
