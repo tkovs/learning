5eb72c8f053cbf9096d455e9cf4386a3ed39b245
tkovs <tkovs@outlook.com>
2018-07-30 10:35:55 -0300

    Go . Slices

--- go/08-arrays/arrays.go
+++ go/08-arrays/arrays.go
@@ -16,12 +16,12 @@ func main() {
 	b := [5]int{1, 2, 3, 4, 5}
 	fmt.Println("dcl:", b)
 
-	var threeD [3][4]int
+	var twoD [3][4]int
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 4; j++ {
-			threeD[i][j] = i + j
+			twoD[i][j] = i + j
 		}
 	}
 
-	fmt.Println("2d: ", threeD)
+	fmt.Println("2d: ", twoD)
 }
