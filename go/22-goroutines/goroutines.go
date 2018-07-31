a45c245247d69f80e3c1c33316f18a0d9709d276
tkovs <tkovs@outlook.com>
2018-07-31 09:42:25 -0300

    Go . Goroutines

---
+++ go/22-goroutines/goroutines.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+		<-time.After(100 * time.Millisecond)
+	}
+}
+
+func main() {
+	f("direct")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	fmt.Scanln()
+	fmt.Println("done")
+}
